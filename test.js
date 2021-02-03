545a543eeb876b65029a339f80760d0507ea3879
wbne <firetruckbw@gmail.com>
2021-02-02 19:04:19 -0600

    finished the mvp
    linked everything together to become a cohesive duct taped product.
    next is making it less ugly and fixing all the bugs/problems :)

--- test.js
+++ test.js
@@ -5,9 +5,16 @@ var boring = false
 var varNum = 0
 var selectedValue = 0
 var newFile = true
+var selectedData = []
+const graphWidth  = (window.innerWidth || document.documentElement.clientWidth ||
+document.body.clientWidth) / 2;
+const graphHeight = (window.innerHeight|| document.documentElement.clientHeight||
+document.body.clientHeight) / 2;
 
 function fileSubmitted()
 {
+  clearGraphs()
+  uncheckRadio()
   file = document.getElementById("myFile");
   newFile = true
   document.getElementById("varWrapper").hidden = true
@@ -25,23 +32,61 @@ function fileSubmitted()
 function variableCount()
 {
   selectedValue = "test"
+  uncheckCheckbox()
   rbs = document.querySelectorAll('input[name="radio"]');
             for (const rb of rbs) {
                 if (rb.checked) {
                     selectedValue = rb.value;
-                    if(newFile)
+                    disableGraphs()
+                    var div = document.getElementById('variables');
+                    while(div.firstChild){
+                      div.removeChild(div.firstChild);
+                    }
+                    if(selectedValue == 3)
+                    {document.getElementById("varWrapper").hidden = true}
+                    else
                     {
-                      var div = document.getElementById('variables');
-                      while(div.firstChild){
-                        div.removeChild(div.firstChild);
-                      }
                       document.getElementById("varWrapper").hidden = false
                       loadVariables()
-                      newFile = false
                     }
                     break;
                 }
             }
+}
+
+function disableGraphs()
+{
+  oneBut = document.querySelectorAll('[id=one]')
+  for(i = 0; i < oneBut.length; i++){
+    if(selectedValue != 1)
+    {oneBut[i].disabled = true}
+    else
+    {oneBut[i].disabled = false}
+  }
+
+  twoBut = document.querySelectorAll('#two')
+  for(i = 0; i < twoBut.length; i++){
+    if(selectedValue != 2)
+    {twoBut[i].disabled = true}
+    else
+    {twoBut[i].disabled = false}
+  }
+
+  allBut = document.querySelectorAll('#all')
+  for(i = 0; i < allBut.length; i++){
+    if(selectedValue != -1)
+    {allBut[i].disabled = true}
+    else
+    {allBut[i].disabled = false}
+  }
+
+  textBut = document.querySelectorAll('#text')
+  for(i = 0; i < textBut.length; i++){
+    if(selectedValue != 3)
+    {textBut[i].disabled = true}
+    else
+    {textBut[i].disabled = false}
+  }
 }
 
 function checkCheckbox()
@@ -56,9 +101,28 @@ function checkCheckbox()
                     if(varNum > selectedValue)
                     {
                       cb.checked = false
+                      varNum = selectedValue
                     }
                 }
             }
+}
+
+function uncheckCheckbox()
+{
+  cbs = document.querySelectorAll('#varOption');
+            for (const cb of cbs)
+            {
+                if (cb.checked)
+                {cb.checked = false}
+            }
+}
+
+function uncheckRadio()
+{
+  rbs = document.querySelectorAll('input[name="radio"]');
+            for (const rb of rbs) {rb.checked = false}
+  bbs = document.querySelectorAll('button');
+            for(const b of bbs) {b.disabled = false}
 }
 
 function loadVariables()
@@ -71,6 +135,7 @@ function loadVariables()
     box = document.createElement('input')
     box.setAttribute('type', 'checkbox')
     box.setAttribute('id', 'varOption')
+    box.setAttribute('name', i)
     box.onclick = checkCheckbox
     lab = document.createElement('label')
     lab.textContent = data.columns[i]
@@ -83,13 +148,21 @@ function loadVariables()
 function clearGraphs()
 {
   d3.select(g).html("")
+  d3.select("#legend").html("")
+  selectedData = []
+  cbs = document.querySelectorAll('#varOption');
+            for (const cb of cbs)
+            {
+                if (cb.checked)
+                {selectedData.push(cb.name)}
+            }
 }
 function boxplot()
 {
   clearGraphs()
   var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 400 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = graphWidth - margin.left - margin.right,
+    height = graphHeight - margin.top - margin.bottom;
 
   var svg = d3.select(g)
   .append("svg")
@@ -100,15 +173,15 @@ function boxplot()
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-  var data = rawText.split(",").map(Number) //might include other delimiters in the future as options
+  var data = d3.csvParse(rawText)
 
-  var data_sorted = data.sort(d3.ascending)
-  var q1 = d3.quantile(data_sorted, .25)
-  var median = d3.quantile(data_sorted, .5)
-  var q3 = d3.quantile(data_sorted, .75)
+  var max = d3.max(data, function(d) {return +d[data.columns[selectedData]]})
+  var min = d3.min(data, function(d) {return +d[data.columns[selectedData]]})
+  var median = (max + min) / 2
+  var q1 = min + (median/2)
+  var q3 = max - (median/2)
   var interQuantileRange = q3 - q1
-  var min = data_sorted[0]
-  var max = data_sorted[data_sorted.length - 1]
+
 
   var y = d3.scaleLinear()
     .domain([min,max])
@@ -152,8 +225,8 @@ function barplot()
   clearGraphs()
   // set the dimensions and margins of the graph
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = graphWidth - margin.left - margin.right,
+    height = graphHeight - margin.top - margin.bottom;
 
 var svg = d3.select(g)
   .append("svg")
@@ -168,7 +241,7 @@ data = d3.csvParse(rawText)
 
 var x = d3.scaleBand()
   .range([ 0, width ])
-  .domain(data.map(function(d) { return d[data.columns[0]]; }))
+  .domain(data.map(function(d) { return d[data.columns[selectedData[0]]]; }))
   .padding(0.2);
 svg.append("g")
   .attr("transform", "translate(0," + height + ")")
@@ -178,7 +251,7 @@ svg.append("g")
     .style("text-anchor", "end");
 
 var y = d3.scaleLinear()
-  .domain([0, d3.max(data, function(d) {return +d[data.columns[1]]})])
+  .domain([0, d3.max(data, function(d) {return +d[data.columns[selectedData[1]]]})])
   .range([ height, 0]);
 svg.append("g")
   .call(d3.axisLeft(y));
@@ -187,10 +260,10 @@ svg.selectAll("mybar")
   .data(data)
   .enter()
   .append("rect")
-    .attr("x", function(d) { return x(d[data.columns[0]]); })
-    .attr("y", function(d) { return y(d[data.columns[1]]); })
+    .attr("x", function(d) { return x(d[data.columns[selectedData[0]]]); })
+    .attr("y", function(d) { return y(d[data.columns[selectedData[1]]]); })
     .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d[data.columns[1]]); })
+    .attr("height", function(d) { return height - y(d[data.columns[selectedData[1]]]); })
     .attr("fill", "#69b3a2")
 }
 
@@ -198,8 +271,8 @@ function dotplot()
 {
   clearGraphs()
   var margin = {top: 10, right: 30, bottom: 30, left: 60},
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+      width = graphWidth - margin.left - margin.right,
+      height = graphHeight - margin.top - margin.bottom;
 
   var svg = d3.select(g)
     .append("svg")
@@ -212,14 +285,14 @@ function dotplot()
 
     data = d3.csvParse(rawText)
     var x = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) {return +d[data.columns[0]]} )])
+      .domain([0, d3.max(data, function(d) {return +d[data.columns[selectedData[0]]]} )])
       .range([ 0, width ]);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
     var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) {return +d[data.columns[1]]} )])
+      .domain([0, d3.max(data, function(d) {return +d[data.columns[selectedData[1]]]} )])
       .range([ height, 0]);
     svg.append("g")
       .call(d3.axisLeft(y));
@@ -228,21 +301,41 @@ function dotplot()
       .data(data)
       .enter()
       .append("circle")
-        .attr("cx", function (d) { return x(d[data.columns[0]]); } )
-        .attr("cy", function (d) { return y(d[data.columns[1]]); } )
+        .attr("cx", function (d) { return x(d[data.columns[selectedData[0]]]); } )
+        .attr("cy", function (d) { return y(d[data.columns[selectedData[1]]]); } )
         .attr("r", 4)
         .style("fill", "#69b3a2")
 }
 
 function wordcloud()
 {
+  clearGraphs()
+
   // List of words
-var myWords = ["Hello", "Everybody", "How", "Are", "You", "Today", "It", "Is", "A", "Lovely", "Day", "I", "Love", "Coding", "In", "My", "Van", "Mate"]
+var myWords = rawText
+  .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+  .replace(/\s\s+/g, ' ')
+  .split(" ")
+var freqMap = {}
+  myWords.forEach(function(w) {
+    if (!freqMap[w]) {
+            freqMap[w] = 0;
+        }
+        freqMap[w] += 1;
+  })
+
+var orig = {}
+myWords.forEach(function(w){
+  if(!orig[w]) {
+    orig[w] = 1
+  }
+})
+myWords = Object.keys(orig)
 
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
-    width = 450 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+    width = graphWidth - margin.left - margin.right,
+    height = graphHeight - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select(g).append("svg")
@@ -255,9 +348,10 @@ var svg = d3.select(g).append("svg")
 // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
 var layout = d3.layout.cloud()
   .size([width, height])
-  .words(myWords.map(function(d) { return {text: d}; }))
-  .padding(10)
-  .fontSize(60)
+  .words(myWords.map(function(d) { return {text: d, size: (freqMap[d] * 5), color: [Math.random()*128+64, Math.random()*128+64, Math.random()*128+64]}; }))
+  .padding(3)
+  .fontSize(function(d) {return d.size})
+  .font("Comic Sans MS")
   .on("end", draw);
 layout.start();
 
@@ -270,7 +364,9 @@ function draw(words) {
       .selectAll("text")
         .data(words)
       .enter().append("text")
-        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-size", function(d) {return d.size + "px"})
+        .style("font-family", "Comic Sans MS")
+        .style("fill", function(d) {return "rgb("+d.color[0]+","+d.color[1]+","+d.color[2]+")"})
         .attr("text-anchor", "middle")
         .attr("transform", function(d) {
           return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
@@ -285,8 +381,8 @@ function streamline()
 
   // set the dimensions and margins of the graph
 var margin = {top: 20, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = graphWidth - margin.left - margin.right,
+    height = graphHeight - margin.top - margin.bottom;
 
 
 // append the svg object to the body of the page
@@ -320,8 +416,8 @@ var svg = d3.select(g)
     var mousemove = function(d) {
       Tooltip
         .html(d.key)
-        .style("left", (d3.mouse(this)[0]+70) + "px")
-        .style("top", (d3.mouse(this)[1]) + "px")
+        .style("left", (event.pageX + 20) + "px")
+        .style("top", (event.pageY - 10) + "px")
     }
     var mouseleave = function(d) {
       Tooltip
@@ -368,10 +464,15 @@ data = d3.csvParse(rawText)
   svg.append("g")
     .call(d3.axisLeft(y));
 
-  // color palette
+  // colors
+  tempColors = []
+  for(i = 0; i < keys.length; i++)
+  {
+    tempColors.push("rgba("+ Math.round(Math.random()*128+128)+","+Math.round(Math.random()*128+128)+","+Math.round(Math.random()*128+128)+",.9)")
+  }
   var color = d3.scaleOrdinal()
     .domain(keys)
-    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf'])
+    .range(tempColors)
 
   var stackedData = d3.stack()
     .offset(d3.stackOffsetSilhouette)
@@ -385,6 +486,17 @@ data = d3.csvParse(rawText)
     boring = false
   }
 
+  divlegend = document.getElementById("legend")
+  for(i = 0; i < keys.length; i++)
+  {
+    legend = document.createElement("p")
+    legend.textContent = keys[i]
+    legend.style.backgroundColor = color(keys[i])
+    legend.style.width = "400px"
+    legend.style.padding = "10px"
+
+    divlegend.append(legend)
+  }
 
   // Show the areas
   svg
