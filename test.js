0a0cc284302cd97da6f11de0259fccabb760f672
wbne <firetruckbw@gmail.com>
2021-02-07 19:51:21 -0600

    added css and extra graph
    fixed up some stuff, styled the page, and added a line graph. TODO: fix word cloud because it's mega scuffed.

--- test.js
+++ test.js
@@ -10,48 +10,34 @@ const graphWidth  = (window.innerWidth || document.documentElement.clientWidth |
 document.body.clientWidth) / 2;
 const graphHeight = (window.innerHeight|| document.documentElement.clientHeight||
 document.body.clientHeight) / 2;
+disableGraphs()
 
 function fileSubmitted()
 {
   clearGraphs()
-  uncheckRadio()
   file = document.getElementById("myFile");
   newFile = true
   document.getElementById("varWrapper").hidden = true
+  var div = document.getElementById('variables')
+  while(div.firstChild){div.removeChild(div.firstChild)}
 
   var rawFile = file.files[0]
   var reader = new FileReader()
   reader.addEventListener('load', function(e){
       let text = e.target.result
       rawText = text
+      loadVariables() //test
+
+      document.getElementById("varWrapper").hidden = false //test
   })
   reader.readAsText(rawFile)
 
 }
 
-function variableCount()
+function clearPrevious()
 {
-  selectedValue = "test"
-  uncheckCheckbox()
-  rbs = document.querySelectorAll('input[name="radio"]');
-            for (const rb of rbs) {
-                if (rb.checked) {
-                    selectedValue = rb.value;
-                    disableGraphs()
-                    var div = document.getElementById('variables');
-                    while(div.firstChild){
-                      div.removeChild(div.firstChild);
-                    }
-                    if(selectedValue == 3)
-                    {document.getElementById("varWrapper").hidden = true}
-                    else
-                    {
-                      document.getElementById("varWrapper").hidden = false
-                      loadVariables()
-                    }
-                    break;
-                }
-            }
+  rawFile = ""
+  rawText = ""
 }
 
 function disableGraphs()
@@ -74,7 +60,7 @@ function disableGraphs()
 
   allBut = document.querySelectorAll('#all')
   for(i = 0; i < allBut.length; i++){
-    if(selectedValue != -1)
+    if(selectedValue < 3)
     {allBut[i].disabled = true}
     else
     {allBut[i].disabled = false}
@@ -82,7 +68,7 @@ function disableGraphs()
 
   textBut = document.querySelectorAll('#text')
   for(i = 0; i < textBut.length; i++){
-    if(selectedValue != 3)
+    if(selectedValue != -1)
     {textBut[i].disabled = true}
     else
     {textBut[i].disabled = false}
@@ -92,37 +78,33 @@ function disableGraphs()
 function checkCheckbox()
 {
   varNum = 0
+  selectedValue = 0 //test
   cbs = document.querySelectorAll('#varOption');
-            for (const cb of cbs)
-            {
-                if (cb.checked)
-                {
-                    varNum = varNum + 1
-                    if(varNum > selectedValue)
-                    {
-                      cb.checked = false
-                      varNum = selectedValue
-                    }
-                }
-            }
+ for (const cb of cbs)
+ {
+      if (cb.checked)
+      {selectedValue++}
+ }
+    disableGraphs() //test
 }
 
-function uncheckCheckbox()
+function uncheckCheckbox(checkers)
 {
+  selectedValue = 0
   cbs = document.querySelectorAll('#varOption');
-            for (const cb of cbs)
-            {
-                if (cb.checked)
-                {cb.checked = false}
-            }
+  for (const cb of cbs)
+  {
+      cb.checked = !checkers
+      if(!checkers)
+      {selectedValue++}
+  }
+  disableGraphs()
 }
 
-function uncheckRadio()
+function textOption()
 {
-  rbs = document.querySelectorAll('input[name="radio"]');
-            for (const rb of rbs) {rb.checked = false}
-  bbs = document.querySelectorAll('button');
-            for(const b of bbs) {b.disabled = false}
+  selectedValue = -1
+  wordcloud()
 }
 
 function loadVariables()
@@ -132,14 +114,18 @@ function loadVariables()
   container = document.getElementById("variables")
   for(i = 0; i < count; i++)
   {
+    lab = document.createElement('label')
+    lab.textContent = data.columns[i]
+    lab.setAttribute("class", "checkLabel")
+    spa = document.createElement('span')
+    spa.setAttribute("class", "checkSpan")
     box = document.createElement('input')
     box.setAttribute('type', 'checkbox')
     box.setAttribute('id', 'varOption')
     box.setAttribute('name', i)
     box.onclick = checkCheckbox
-    lab = document.createElement('label')
-    lab.textContent = data.columns[i]
-    container.append(box)
+    lab.append(box)
+    lab.append(spa)
     container.append(lab)
     container.append(document.createElement('br'))
   }
@@ -149,6 +135,7 @@ function clearGraphs()
 {
   d3.select(g).html("")
   d3.select("#legend").html("")
+  document.getElementById("wrapper").style.visibility = "hidden";
   selectedData = []
   cbs = document.querySelectorAll('#varOption');
             for (const cb of cbs)
@@ -157,6 +144,12 @@ function clearGraphs()
                 {selectedData.push(cb.name)}
             }
 }
+
+function unwrap()
+{
+  document.getElementById("wrapper").style.visibility = "visible";
+}
+
 function boxplot()
 {
   clearGraphs()
@@ -382,7 +375,7 @@ function streamline()
   // set the dimensions and margins of the graph
 var margin = {top: 20, right: 30, bottom: 30, left: 60},
     width = graphWidth - margin.left - margin.right,
-    height = graphHeight - margin.top - margin.bottom;
+    height = 1.3*graphHeight - margin.top - margin.bottom;
 
 
 // append the svg object to the body of the page
@@ -431,18 +424,30 @@ var svg = d3.select(g)
 data = d3.csvParse(rawText)
 
   // List of groups = header of the csv files
-  var keys = data.columns.slice(1)
-  temp_max = d3.max(data, function(d) {return +d[data.columns[1]]})
+  var keys = []
+  for(i = 1; i < selectedData.length; i++)
+  {keys.push(data.columns[selectedData[i]])}
+
+  total_max = 0
+  for(i = 1; i < selectedData.length; i++)
+  {total_max += d3.max(data, function(d) {return +d[data.columns[i]]})}
   max_time = d3.max(data, function(d) {return +d[data.columns[0]]})
   min_time = d3.min(data, function(d) {return +d[data.columns[0]]})
   avg_time = (max_time + min_time) / 2
+  q1 = (min_time+avg_time)/2
+  q3 = (max_time+avg_time)/2
+  o1 = (min_time+q1)/2
+  o2 = (q1+avg_time)/2
+  o3 = (q3+avg_time)/2
+  o4 = (max_time+q3)/2
+
   // Add X axis
   var x = d3.scaleLinear()
     .domain([min_time, max_time])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSize(-height).tickValues([min_time, min_time+avg_time/2, avg_time, max_time-avg_time/2, max_time]))
+    .call(d3.axisBottom(x).tickSize(-height).tickValues([min_time, o1, q1, o2, avg_time, o3, q3, o4, max_time]))
     .select(".domain").remove()
   svg.selectAll(".tick line").attr("stroke", "#b8b8b8")
   svg.append("text")
@@ -453,12 +458,12 @@ data = d3.csvParse(rawText)
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([-.5*keys.length*temp_max, .5*keys.length*temp_max])
+    .domain([-.5*total_max, .5*total_max])
     .range([ height, 0 ]);
   if(boring)
   {
     y = d3.scaleLinear()
-      .domain([0, keys.length*temp_max])
+      .domain([0, total_max])
       .range([ height, 0 ]);
   }
   svg.append("g")
@@ -492,7 +497,7 @@ data = d3.csvParse(rawText)
     legend = document.createElement("p")
     legend.textContent = keys[i]
     legend.style.backgroundColor = color(keys[i])
-    legend.style.width = "400px"
+    legend.style.width = "200px"
     legend.style.padding = "10px"
 
     divlegend.append(legend)
@@ -520,4 +525,104 @@ function stacked()
 {
   boring = true
   streamline(true)
+}
+
+function linegraph()
+{
+  clearGraphs()
+  // set the dimensions and margins of the graph
+  var margin = {top: 20, right: 30, bottom: 30, left: 60},
+      width = graphWidth - margin.left - margin.right,
+      height = 1.3*graphHeight - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select(g)
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+data = d3.csvParse(rawText)
+
+// List of groups = header of the csv files
+var keys = []
+for(i = 1; i < selectedData.length; i++)
+{keys.push(data.columns[selectedData[i]])}
+
+total_max = 0
+for(i = 1; i < selectedData.length; i++)
+{
+  tmep = d3.max(data, function(d) {return +d[data.columns[i]]})
+  if(total_max < tmep)
+  {total_max = tmep}
+}
+max_time = d3.max(data, function(d) {return +d[data.columns[0]]})
+min_time = d3.min(data, function(d) {return +d[data.columns[0]]})
+
+// colors
+tempColors = []
+for(i = 0; i < keys.length; i++)
+{
+  tempColors.push("rgba("+ Math.round(Math.random()*128+128)+","+Math.round(Math.random()*128+128)+","+Math.round(Math.random()*128+128)+",.9)")
+}
+var color = d3.scaleOrdinal()
+  .domain(keys)
+  .range(tempColors)
+
+  // group the data: I want to draw one line per group
+  //var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+  //  .keys(keys)
+  //  .entries(data);
+
+  // Add X axis --> it is a date format
+  var x = d3.scaleLinear()
+    .domain(d3.extent(data, function(d) {return +d[data.columns[0]]; }))
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).ticks(5));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, total_max])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+for(i = 0; i < data.length; i++)
+{
+  for(j = 0; j < keys.length; j++)
+  {//console.log(data[i][keys[j]])
+  }
+}
+  // Draw the line
+  for(i = 1; i < keys.length; i++)
+  {
+  svg.selectAll(".line")
+      .data([data])
+      .enter()
+      .append("path")
+        .attr("d", d3.line()
+          .x(function(d){return x(+d[data.columns[selectedData[0]]]) })
+          .y(function(d){return y(+d[data.columns[selectedData[i]]]) })
+      )
+        .attr("fill", "none")
+        .attr("stroke", function(d){ return tempColors[i] })
+        .attr("stroke-width", 3)
+  }
+
+  divlegend = document.getElementById("legend")
+  for(i = 0; i < keys.length; i++)
+  {
+    legend = document.createElement("p")
+    legend.textContent = keys[i]
+    legend.style.backgroundColor = color(keys[i])
+    legend.style.width = "200px"
+    legend.style.padding = "10px"
+
+    divlegend.append(legend)
+  }
 }
